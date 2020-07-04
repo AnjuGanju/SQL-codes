ad7b30a4ab8336c05feddd9c6fba911c0a24ab7f
anjuganju <38179793+AnjuGanju@users.noreply.github.com>
2020-07-04 13:24:35 -0700

    Leetcode updates

---
+++ Leetcode/1407. Top Travellers.sql
@@ -0,0 +1,39 @@
+/*
+Table: Users
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| name          | varchar |
++---------------+---------+
+id is the primary key for this table.
+name is the name of the user.
+
+
+Table: Rides
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| user_id       | int     |
+| distance      | int     |
++---------------+---------+
+id is the primary key for this table.
+user_id is the id of the user who travelled the distance "distance".
+
+
+Write an SQL query to report the distance travelled by each user.
+
+Return the result table ordered by travelled_distance in descending order, if two or more users travelled the same distance, order them by their name in ascending order.
+
+The query result format is in the following example.
+*/
+
+SELECT u.name, IFNULL(SUM(r.distance),0) as travelled_distance
+FROM users u
+LEFT JOIN rides r
+ON u.id = r.user_id
+GROUP BY r.user_id
+ORDER BY SUM(r.distance) DESC,u.name
