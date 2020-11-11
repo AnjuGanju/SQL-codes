180a045c5b8c3142d9300311607a2e06af8c7a5e
anjuganju <38179793+AnjuGanju@users.noreply.github.com>
2020-11-10 21:02:59 -0800

    updated 10th Nov

---
+++ Leetcode/Leetcode/Medium/608. Tree Node.sql
@@ -0,0 +1,42 @@
+/*
+SQL Schema
+Table: Candidate
+
++-----+---------+
+| id  | Name    |
++-----+---------+
+| 1   | A       |
+| 2   | B       |
+| 3   | C       |
+| 4   | D       |
+| 5   | E       |
++-----+---------+
+Table: Vote
+
++-----+--------------+
+| id  | CandidateId  |
++-----+--------------+
+| 1   |     2        |
+| 2   |     4        |
+| 3   |     3        |
+| 4   |     2        |
+| 5   |     5        |
++-----+--------------+
+id is the auto-increment primary key,
+CandidateId is the id appeared in Candidate table.
+Write a sql to find the name of the winning candidate, the above example will return the winner B.
+
++------+
+| Name |
++------+
+| B    |
++------+
+*/
+
+SELECT name
+FROM candidate
+WHERE id = (SELECT candidateid
+FROM vote
+GROUP BY candidateid
+ORDER BY COUNT(candidateid) DESC
+LIMIT 1)
